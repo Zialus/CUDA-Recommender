15245ce8454e213207e81451ed65f5b7efd376b1
Raul Ferreira <raulmendesferreira@gmail.com>
2018-08-16 01:03:37 +0100

    General Improvements to travis config.

--- .travis.linux.sh
+++ .travis.linux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
   
-##### INSTALL CMAKE 3.11.3 #####
+##### INSTALL RECENT CMAKE #####
 echo "Installing CMAKE"
-wget https://cmake.org/files/v3.11/cmake-3.11.3-Linux-x86_64.sh
-sudo sh cmake-3.11.3-Linux-x86_64.sh --prefix=/home/travis/.local/ --exclude-subdir
+wget https://cmake.org/files/v3.12/cmake-3.12.1-Linux-x86_64.sh
+sudo sh cmake-3.12.1-Linux-x86_64.sh --prefix=/home/travis/.local/ --exclude-subdir
 
 ##### INSTALL CUDA #####
 echo "Installing CUDA library"
@@ -12,15 +12,15 @@ echo "Installing CUDA library"
 case ${CUDA:0:3} in
 
 '6.5')
-    travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_6.5-14_amd64.deb
-    travis_retry sudo dpkg -i cuda-repo-ubuntu1404_6.5-14_amd64.deb
+    travis_retry wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_6.5-19_amd64.deb
+    travis_retry sudo dpkg -i cuda-repo-ubuntu1404_6.5-19_amd64.deb
     ;;
 '7.0')
-    travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.0-28_amd64.deb
+    travis_retry wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.0-28_amd64.deb
     travis_retry sudo dpkg -i cuda-repo-ubuntu1404_7.0-28_amd64.deb
     ;;
 '7.5')
-    travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
+    travis_retry wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
     travis_retry sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
     ;;
 '8.0')
@@ -28,18 +28,18 @@ case ${CUDA:0:3} in
     travis_retry sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
     ;;
 '9.0')
-    travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+    travis_retry wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
     travis_retry sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
     travis_retry sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
     ;;
 '9.1')
-    travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
+    travis_retry wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
     travis_retry sudo dpkg -i cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
     travis_retry sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
     ;;
 '9.2')
-    travis_retry wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.2.88-1_amd64.deb
-    travis_retry sudo dpkg -i cuda-repo-ubuntu1604_9.2.88-1_amd64.deb
+    travis_retry wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.2.148-1_amd64.deb
+    travis_retry sudo dpkg -i cuda-repo-ubuntu1604_9.2.148-1_amd64.deb
     travis_retry sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
     ;;
 
--- .travis.osx.sh
+++ .travis.osx.sh
@@ -1,17 +1,16 @@
 #!/bin/bash -uxe
 
-brew upgrade cmake
-
 case ${CUDA:0:3} in
 
-'7.5')  installer="http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda_7.5.27_mac.dmg";;
+'7.5')  installer="https://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda_7.5.27_mac.dmg";;
 '8.0')  installer="https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_mac-dmg";;
 '9.0')  installer="https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_mac-dmg";;
 '9.1')  installer="https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.128_mac";;
-'9.2')  installer="https://developer.nvidia.com/compute/cuda/9.2/Prod/local_installers/cuda_9.2.64_mac";;
+'9.2')  installer="https://developer.nvidia.com/compute/cuda/9.2/Prod2/local_installers/cuda_9.2.148_mac";;
 
 esac
 
+brew install wget
 wget -O cuda.dmg "$installer"
 
 brew install p7zip
