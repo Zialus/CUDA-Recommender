143a81f2d5f5245d5dc4b998844bacb940c9c30d
Raul Ferreira <raulmendesferreira@gmail.com>
2019-01-22 18:50:02 +0000

    More formatting improvements.

--- src/ALS.cpp
+++ src/ALS.cpp
@@ -24,7 +24,7 @@ void choldc1(int n, float** a, float* p) {
 }
 
 void choldcsl(int n, float** A) {
-    float* p = (float *)malloc(n * sizeof(float));
+    float* p = (float*) malloc(n * sizeof(float));
     choldc1(n, A, p);
     for (int i = 0; i < n; ++i) {
         A[i][i] = 1 / p[i];
@@ -66,12 +66,12 @@ void inverseMatrix_CholeskyMethod(int n, float** A) {
 }
 
 //Multiply matrix M transpose by M 
-void Mt_byM_multiply(int i, int j, float**M, float**Result){
+void Mt_byM_multiply(int i, int j, float** M, float** Result) {
     float SUM;
-    for (int I = 0; I < j; ++I){
+    for (int I = 0; I < j; ++I) {
         for (int J = I; J < j; ++J) {
             SUM = 0.0f;
-            for (int K = 0; K < i; ++K){
+            for (int K = 0; K < i; ++K) {
                 //printf("%.3f %.3f\n", M[K][I], M[K][J]);
                 SUM += M[K][I] * M[K][J];
             }
@@ -82,20 +82,21 @@ void Mt_byM_multiply(int i, int j, float**M, float**Result){
 }
 
 //Multiply matrix M by M tranpose
-void M_byMt_multiply(int i, int j, float**M, float**Result){
+void M_byMt_multiply(int i, int j, float** M, float** Result) {
     float SUM;
-    for (int I = 0; I < i; ++I){
+    for (int I = 0; I < i; ++I) {
         for (int J = 0; J < i; ++J) {
             SUM = 0.0;
-            for (int K = 0; K < j; ++K)
+            for (int K = 0; K < j; ++K) {
                 SUM += M[I][K] * M[J][K];
+            }
             Result[I][J] = SUM;
         }
     }
 }
 
-void ALS(smat_t &R, mat_t &W, mat_t &H, testset_t &T, parameter &param){
-    if (param.enable_cuda){
+void ALS(smat_t& R, mat_t& W, mat_t& H, testset_t& T, parameter& param) {
+    if (param.enable_cuda) {
         printf("CUDA enabled version.\n");
 
         params_als parameters;
@@ -110,34 +111,46 @@ void ALS(smat_t &R, mat_t &W, mat_t &H, testset_t &T, parameter &param){
         parameters.nThreadsPerBlock = param.nThreadsPerBlock;
 
         smat_t_C_als R_C;
-        R_C.cols = R.cols; R_C.rows = R.rows; R_C.nnz = R.nnz;
-        R_C.val = R.val; R_C.val_t = R.val_t;
-        R_C.nbits_val = R.nbits_val; R_C.nbits_val_t = R.nbits_val_t;
+        R_C.cols = R.cols;
+        R_C.rows = R.rows;
+        R_C.nnz = R.nnz;
+        R_C.val = R.val;
+        R_C.val_t = R.val_t;
+        R_C.nbits_val = R.nbits_val;
+        R_C.nbits_val_t = R.nbits_val_t;
         R_C.with_weights = R.with_weights;
-        R_C.weight = R.weight; R_C.weight_t = R.weight_t;
-        R_C.nbits_weight = R.nbits_weight; R_C.nbits_weight_t = R.nbits_weight_t;
-        R_C.col_ptr = R.col_ptr; R_C.row_ptr = R.row_ptr;
-        R_C.nbits_col_ptr = R.nbits_col_ptr; R_C.nbits_row_ptr = R.nbits_row_ptr;
-        R_C.col_idx = R.col_idx; R_C.row_idx = R.row_idx;
-        R_C.nbits_col_idx = R.nbits_col_idx; R_C.nbits_row_idx = R.nbits_row_idx;
-        R_C.max_col_nnz = R.max_col_nnz; R_C.max_row_nnz = R.max_row_nnz;
-        R_C.colMajored_sparse_idx = R.colMajored_sparse_idx; R_C.nbits_colMajored_sparse_idx = R.nbits_colMajored_sparse_idx;
+        R_C.weight = R.weight;
+        R_C.weight_t = R.weight_t;
+        R_C.nbits_weight = R.nbits_weight;
+        R_C.nbits_weight_t = R.nbits_weight_t;
+        R_C.col_ptr = R.col_ptr;
+        R_C.row_ptr = R.row_ptr;
+        R_C.nbits_col_ptr = R.nbits_col_ptr;
+        R_C.nbits_row_ptr = R.nbits_row_ptr;
+        R_C.col_idx = R.col_idx;
+        R_C.row_idx = R.row_idx;
+        R_C.nbits_col_idx = R.nbits_col_idx;
+        R_C.nbits_row_idx = R.nbits_row_idx;
+        R_C.max_col_nnz = R.max_col_nnz;
+        R_C.max_row_nnz = R.max_row_nnz;
+        R_C.colMajored_sparse_idx = R.colMajored_sparse_idx;
+        R_C.nbits_colMajored_sparse_idx = R.nbits_colMajored_sparse_idx;
 
-        float **W_c;
-        float **H_c;
+        float** W_c;
+        float** H_c;
 
-        H_c = (float **)malloc(R.cols * sizeof(float *));
+        H_c = (float**) malloc(R.cols * sizeof(float*));
         assert(H_c);
 
-        for (int i = 0; i < R.cols; i++){
+        for (int i = 0; i < R.cols; i++) {
             H_c[i] = &H[i][0];
             assert(H_c[i]);
         }
 
-        W_c = (float **)malloc(R.rows * sizeof(float *));
+        W_c = (float**) malloc(R.rows * sizeof(float*));
         assert(W_c);
 
-        for (int i = 0; i < R.rows; i++){
+        for (int i = 0; i < R.rows; i++) {
             W_c[i] = &W[i][0];
             assert(W_c[i]);
         }
@@ -146,14 +159,13 @@ void ALS(smat_t &R, mat_t &W, mat_t &H, testset_t &T, parameter &param){
 
         free(W_c);
         free(H_c);
-    }
-    else{
+    } else {
         ALS_multicore(R, W, H, param);
     }
 }
 
 
-void ALS_multicore(smat_t &R, mat_t &W, mat_t &H, parameter &param){
+void ALS_multicore(smat_t& R, mat_t& W, mat_t& H, parameter& param) {
     int maxIter = param.maxiter;
     float lambda = param.lambda;
     int k = param.k;
@@ -161,26 +173,26 @@ void ALS_multicore(smat_t &R, mat_t &W, mat_t &H, parameter &param){
 
     omp_set_num_threads(param.threads);
 
-    for (int iter = 0; iter < maxIter; ++iter){
+    for (int iter = 0; iter < maxIter; ++iter) {
 
         //optimize W over H
 #pragma omp parallel for schedule(kind)
-        for (int Rw = 0; Rw < R.rows; ++Rw){
-            float *Wr = &W[Rw][0];
+        for (int Rw = 0; Rw < R.rows; ++Rw) {
+            float* Wr = &W[Rw][0];
             int omegaSize = R.row_ptr[Rw + 1] - R.row_ptr[Rw];
-            float ** subMatrix;
+            float** subMatrix;
 
-            if (omegaSize>0){
-                float * subVector = (float *)malloc(k * sizeof(float));
-                subMatrix = (float **)malloc(k * sizeof(float *));
-                for (int i = 0; i < k; ++i){
-                    subMatrix[i] = (float *)malloc(k * sizeof(float));
+            if (omegaSize > 0) {
+                float* subVector = (float*) malloc(k * sizeof(float));
+                subMatrix = (float**) malloc(k * sizeof(float*));
+                for (int i = 0; i < k; ++i) {
+                    subMatrix[i] = (float*) malloc(k * sizeof(float));
                 }
 
                 //a trick to avoid malloc
-                float ** H_Omega = (float **)malloc(omegaSize * sizeof(float *));
+                float** H_Omega = (float**) malloc(omegaSize * sizeof(float*));
                 unsigned i = 0;
-                for (int idx = R.row_ptr[Rw]; idx < R.row_ptr[Rw + 1]; ++idx){
+                for (int idx = R.row_ptr[Rw]; idx < R.row_ptr[Rw + 1]; ++idx) {
                     H_Omega[i] = &H[R.col_idx[idx]][0];
                     ++i;
                 }
@@ -188,7 +200,7 @@ void ALS_multicore(smat_t &R, mat_t &W, mat_t &H, parameter &param){
                 Mt_byM_multiply(omegaSize, k, H_Omega, subMatrix);
 
                 //add lambda to diag of sub-matrix
-                for (int c = 0; c < k; c++){
+                for (int c = 0; c < k; c++) {
                     subMatrix[c][c] = subMatrix[c][c] + lambda;
                 }
 
@@ -197,32 +209,31 @@ void ALS_multicore(smat_t &R, mat_t &W, mat_t &H, parameter &param){
 
 
                 //sparse multiplication
-                for (int c=0; c < k; ++c){
+                for (int c = 0; c < k; ++c) {
                     subVector[c] = 0;
-                    for (int idx = R.row_ptr[Rw]; idx < R.row_ptr[Rw + 1]; ++idx){
+                    for (int idx = R.row_ptr[Rw]; idx < R.row_ptr[Rw + 1]; ++idx) {
                         unsigned idx2 = R.colMajored_sparse_idx[idx];
                         subVector[c] += R.val[idx2] * H[R.col_idx[idx]][c];
                     }
                 }
 
                 //multiply subVector by subMatrix
-                for (int c = 0; c < k; ++c){
+                for (int c = 0; c < k; ++c) {
                     Wr[c] = 0;
-                    for (int subVid = 0; subVid < k; ++subVid){
+                    for (int subVid = 0; subVid < k; ++subVid) {
                         Wr[c] += subVector[subVid] * subMatrix[c][subVid];
                     }
                 }
 
 
-                for (int i = 0; i < k; ++i){
+                for (int i = 0; i < k; ++i) {
                     free(subMatrix[i]);
                 }
                 free(subMatrix);
                 free(subVector);
                 free(H_Omega);
-            }
-            else{
-                for (int c = 0; c < k; ++c){
+            } else {
+                for (int c = 0; c < k; ++c) {
                     Wr[c] = 0.0f;
                     //printf("%.3f ", Wr[c]);
                 }
@@ -232,22 +243,22 @@ void ALS_multicore(smat_t &R, mat_t &W, mat_t &H, parameter &param){
 
         //optimize H over W
 #pragma omp parallel for schedule(kind)
-        for (int Rh = 0; Rh < R.cols; ++Rh){
-            float *Hr = &H[Rh][0];
+        for (int Rh = 0; Rh < R.cols; ++Rh) {
+            float* Hr = &H[Rh][0];
             unsigned omegaSize = R.col_ptr[Rh + 1] - R.col_ptr[Rh];
-            float ** subMatrix;
+            float** subMatrix;
 
-            if (omegaSize>0){
-                float * subVector = (float *)malloc(k * sizeof(float));
-                subMatrix = (float **)malloc(k * sizeof(float *));
-                for (int i = 0; i < k; ++i){
-                    subMatrix[i] = (float *)malloc(k * sizeof(float));
+            if (omegaSize > 0) {
+                float* subVector = (float*) malloc(k * sizeof(float));
+                subMatrix = (float**) malloc(k * sizeof(float*));
+                for (int i = 0; i < k; ++i) {
+                    subMatrix[i] = (float*) malloc(k * sizeof(float));
                 }
 
                 //a trick to avoid malloc
-                float ** W_Omega = (float **)malloc(omegaSize * sizeof(float *));
+                float** W_Omega = (float**) malloc(omegaSize * sizeof(float*));
                 unsigned i = 0;
-                for (long idx = R.col_ptr[Rh]; idx < R.col_ptr[Rh + 1]; ++idx){
+                for (long idx = R.col_ptr[Rh]; idx < R.col_ptr[Rh + 1]; ++idx) {
                     W_Omega[i] = &W[R.row_idx[idx]][0];
                     ++i;
                 }
@@ -255,7 +266,7 @@ void ALS_multicore(smat_t &R, mat_t &W, mat_t &H, parameter &param){
                 Mt_byM_multiply(omegaSize, k, W_Omega, subMatrix);
 
                 //add lambda to diag of sub-matrix
-                for (int c = 0; c < k; c++){
+                for (int c = 0; c < k; c++) {
                     subMatrix[c][c] = subMatrix[c][c] + lambda;
                 }
 
@@ -264,31 +275,30 @@ void ALS_multicore(smat_t &R, mat_t &W, mat_t &H, parameter &param){
 
 
                 //sparse multiplication
-                for (int c = 0; c < k; ++c){
+                for (int c = 0; c < k; ++c) {
                     subVector[c] = 0;
-                    for (long idx = R.col_ptr[Rh]; idx < R.col_ptr[Rh + 1]; ++idx){
+                    for (long idx = R.col_ptr[Rh]; idx < R.col_ptr[Rh + 1]; ++idx) {
                         subVector[c] += R.val[idx] * W[R.row_idx[idx]][c];
                     }
                 }
 
                 //multiply subVector by subMatrix
-                for (int c = 0; c < k; ++c){
+                for (int c = 0; c < k; ++c) {
                     Hr[c] = 0;
-                    for (int subVid = 0; subVid < k; ++subVid){
+                    for (int subVid = 0; subVid < k; ++subVid) {
                         Hr[c] += subVector[subVid] * subMatrix[c][subVid];
                     }
                 }
 
 
-                for (int i = 0; i < k; ++i){
+                for (int i = 0; i < k; ++i) {
                     free(subMatrix[i]);
                 }
                 free(subMatrix);
                 free(subVector);
                 free(W_Omega);
-            }
-            else{
-                for (int c = 0; c < k; ++c){
+            } else {
+                for (int c = 0; c < k; ++c) {
                     Hr[c] = 0.0f;
                 }
             }
