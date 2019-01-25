f4ac4a14c7460506b78fdde747f3447f8be90847
Raul Ferreira <raulmendesferreira@gmail.com>
2019-01-25 17:50:48 +0000

    Fixed issue with MSVC2013.

--- src/pmf.h
+++ src/pmf.h
@@ -7,20 +7,37 @@ enum class solvertype {CCD, ALS};
 
 class parameter {
 public:
-    solvertype solver_type = solvertype::CCD;
-    int k = 10;
-    int threads = 4;
-    int maxiter = 5;
-    int maxinneriter = 5;
-    float lambda = 0.1f;
-    float eps = 1e-3f;  // for the fundec stop-cond in ccdr1
-    int do_predict = 0;
-    int verbose = 0;
-    int do_nmf = 0;  // non-negative matrix factorization
-    bool enable_cuda = false;
-    int nBlocks = 32;
-    int nThreadsPerBlock = 256;
-    char src_dir[1024] = "../data/simple";
+    solvertype solver_type;
+    int k;
+    int threads;
+    int maxiter;
+    int maxinneriter;
+    float lambda;
+    float eps;
+    int do_predict;
+    int verbose;
+    int do_nmf;
+    bool enable_cuda;
+    int nBlocks;
+    int nThreadsPerBlock;
+    char src_dir[1024];
+
+    parameter() {
+        solver_type = solvertype::CCD;
+        k = 10;
+        threads = 4;
+        maxiter = 5;
+        maxinneriter = 5;
+        lambda = 0.1f;
+        eps = 1e-3f;  // for the fundec stop-cond in ccdr1
+        do_predict = 0;
+        verbose = 0;
+        do_nmf = 0;  // non-negative matrix factorization
+        enable_cuda = false;
+        nBlocks = 32;
+        nThreadsPerBlock = 256;
+        snprintf(src_dir, 1024, "../data/simple");
+    }
 };
 
 #endif
