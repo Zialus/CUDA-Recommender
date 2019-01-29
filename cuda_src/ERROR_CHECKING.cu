dd9d8c76c53dad38561ca68057db141b0e7522d6
Raul Ferreira <raulmendesferreira@gmail.com>
2019-01-29 15:16:58 +0000

    Added RMSE function on GPU side to use later.

---
+++ cuda_src/ERROR_CHECKING.cu
@@ -0,0 +1,32 @@
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+#include <device_launch_parameters.h>
+
+#include "ERROR_CHECKING.h"
+
+__global__ void
+GPU_rmse(long const* __restrict__ test_row, long const* __restrict__ test_col, float const* __restrict__ test_val,
+         float* __restrict__ pred_v, float* __restrict__ rmse, float const* __restrict__ W,
+         float const* __restrict__ H, int m, int k, int rows, int cols, bool ifALS) {
+    int c = blockIdx.x * blockDim.x + threadIdx.x;
+    if (c < m) {
+
+        if (ifALS) {
+            for (int t = 0; t < k; t++) {
+                int i = test_row[c];
+                int j = test_col[c];
+                pred_v[c] += W[i * k + t] * H[j * k + t]; //W[i][t] * H[j][t]
+//                pred_v[c] += W[t * rows + i] * H[t * cols + j]; //W[i][t] * H[j][t];
+            }
+        } else {
+            for (int t = 0; t < k; t++) {
+                int i = test_row[c];
+                int j = test_col[c];
+                pred_v[c] += W[i * t + rows] * H[j * t + cols]; //W[t][i] * H[t][j];
+//                pred_v[c] += W[i * rows + t] * H[j * cols + t]; //W[t][i] * H[t][j], because they are transposed
+
+            }
+        }
+        rmse[c] = (pred_v[c] - test_val[c]) * (pred_v[c] - test_val[c]);
+    }
+}
--- cuda_src/ERROR_CHECKING.h
+++ cuda_src/ERROR_CHECKING.h
@@ -2,6 +2,7 @@
 #define ERROR_CHECKING_H
 
 #include <cassert>
+#include <cstdio>
 
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 
@@ -11,5 +12,10 @@ inline void gpuAssert(cudaError_t code, const char* file, int line) {
         assert(code == cudaSuccess);
     }
 }
+
+__global__ void
+GPU_rmse(long const* __restrict__ test_row, long const* __restrict__ test_col, float const* __restrict__ test_val,
+         float* __restrict__ pred_v, float* __restrict__ rmse, float const* __restrict__ W,
+         float const* __restrict__ H, int m, int k, int rows, int cols, bool ifALS);
 
 #endif //ERROR_CHECKING_H
